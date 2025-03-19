33bc544491c5d1c41f9daca0e540e73223596c96
Wangdali2049 <Ande2049@163.com>
2025-03-19 23:29:52 +0800

    Analysis pipeline

---
+++ 02_Code/Analysis_pipeline.R
@@ -0,0 +1,351 @@
+# 0. Package&Function ----------------------------------------------------------
+library(readxl)
+library(ggplot2)
+library(readr)
+library(dplyr)
+library(openxlsx)
+library(clusterProfiler)
+library(pathview)
+library(org.Hs.eg.db)
+library(ggplot2)
+source("./02_Code/QC_PCA.R")
+source("./02_Code/QC_boxplot.R")
+source("./02_Code/QC_heatmap.R")
+source("./02_Code/run_DE.R")
+source("./02_Code/run_enrichment_analysis.R")
+
+# 1. Data input ----------------------------------------------------------------
+## 1.1 Group input ----
+# 导入分组信息
+data_group <- read_excel("./01_Data/OCI_AML2_group.xlsx")
+table(data_group$group)
+data_group <- as.data.frame(data_group)
+
+#table(data_group$group)
+
+# 配色设置
+# 配色设置
+value_colour <- c("High" = "#E64B35FF",
+                  "Low" = "#4DBBD5FA",
+                  "Con" = "#F2A200")
+rownames(data_group) <- data_group$id
+
+## 1.2 DIA matrix input ----
+fill_norm <- read.csv("./01_Data/report.pg_matrix_fill_norma.csv")
+colnames(fill_norm)
+rownames(fill_norm) <- fill_norm$X
+fill_norm <- fill_norm[,-1]
+OCI_fill_norm <- fill_norm[,grep("OCI",colnames(fill_norm))]
+
+# remove abnormal sample
+data_group <- data_group[-grep("4W",data_group$id),]
+fill_norm <- fill_norm[,-grep("4W",colnames(fill_norm))]
+
+# Protein anno input
+data_anno <- read.xlsx("./01_Data/data_anno.xlsx")
+data_anno <- as.data.frame(data_anno)
+rownames(data_anno) <- data_anno$Protein.Group
+
+# 2. Set output category -------------------------------------------------------------
+#dir.create("./03_Result/GO&KEGG/MOLM13")
+dir_QC <- "./03_Result/QC/OCI_AML2/"
+QC_data <- OCI_fill_norm
+# 3. QC ------------------------------------------------------------------------
+## 3.1 Boxplot -----------------------------------------------------------------
+pdf(file = paste0(dir_QC,"QC_boxplot_normalization.pdf"),
+    width = 6,
+    height = 4)
+QC_boxplot(QC_data,data_group = data_group,
+           value_colour = value_colour,
+           title = "normalized data")
+dev.off()
+## 3.2 Heatmap -----------------------------------------------------------------
+pdf(file = paste0(dir_QC,"QC_heatmap_normalization.pdf"),
+    width = 6,
+    height = 6)
+QC_heatmap(QC_data,data_group = data_group,
+           value_colour = value_colour)
+dev.off()
+
+## 3.3 PCA ---------------------------------------------------------------------
+pdf(file = paste0(dir_QC,"QC_pca_normalization.pdf"),
+    width = 7,
+    height = 7)
+QC_PCA(data = log2(QC_data+1),
+       data_group = data_group,
+       value_colour = value_colour)
+dev.off()
+
+# 4. DE ------------------------------------------------------------------------
+# expr input
+data_fill_norm <- read.csv("./01_Data/report.pg_matrix_fill_norma.csv")
+# 整理数据
+colnames(data_fill_norm)
+rownames(data_fill_norm) <- data_fill_norm$X
+data_fill_norm <- data_fill_norm[,-1]
+
+# anno input
+# 注意，data和data_anno的行名应一致
+data_anno <- read_xlsx("./01_Data/data_anno.xlsx")
+data_anno <- as.data.frame(data_anno)
+colnames(data_anno)
+rownames(data_anno) <- data_anno$Protein.Group
+data_anno <- data_anno[rownames(data_anno)%in%rownames(data_fill_norm),]
+
+data_fill_norm <- data_fill_norm[,order(colnames(data_fill_norm))]
+
+## 4.1 Set output catagory ----
+dir_DE <- "./03_Result/DE/OCI_AML2/"
+
+## 4.2 Set group ----
+data_group <- read.xlsx("./01_Data/IC50_group.xlsx")
+rownames(data_group) <- data_group$id
+data_group <- data_group[order(rownames(data_group)),]
+table(data_group$group)
+targeted_group <- data_group[grep('OCI',data_group$id),]
+
+# 2W/6W-WT 组别
+if(T){
+  targeted_group[grep("2W",targeted_group$id),2] <- gsub("_VEN","_2W",
+                                                         targeted_group[grep("2W",targeted_group$id),2])
+  targeted_group[grep("6W",targeted_group$id),2] <- gsub("_VEN","_6W",
+                                                         targeted_group[grep("6W",targeted_group$id),2])
+  table(targeted_group$group)
+}
+
+
+# 根据分组选择要进行差异分析的组别
+group_1 <- "High"       # group 1为实验组
+group_2 <- "Ctrl"       # group 2为对照组
+
+## 4.3 Res output ----
+result_merge <- run_DE(data = data_fill_norm,
+                       data_group = targeted_group,
+                       data_anno = data_anno,
+                       log2 = TRUE,
+                       group_1 = group_1,
+                       group_2 = group_2,
+                       logfc_threshold = 0.25,           # 对应fc约为1.25
+                       pvalue_threshold = 0.05,
+                       qvalue_threshold = NULL,
+                       dir = "./03_Result/DE/OCI_AML2/")
+# 统计上下调基因数量
+table(result_merge$result_merge$Sig)
+
+## 4.4 Annotated volcano plot ----
+
+# 
+## 5.1 Set output catagory----
+#OCI_AML2/MV4_11/MOLM13
+# 指定文件夹路径
+dir.create("./03_Result/GO&KEGG/MOLM13/VEN_vs_WT/")
+dir_enrich <- "./03_Result/GO&KEGG/OCI_AML2/VEN_vs_WT/"
+
+## 5.2 DE_res input ----
+DE_result <- read.csv('./03_Result/DE/OCI_AML2/OCI_VEN_vs_OCI_WT/result_DE.csv')
+
+## 5.3 set P.Value ----
+GeneSymbol <- subset(DE_result, P.Value < 0.05)
+
+## 5.4 set cutoff值 ----
+cutoff <- 0.5                  # 1.4倍变化
+
+# 转换基因名 
+y <- GeneSymbol$Genes
+gene <- unlist(lapply(y,function(y) strsplit(as.character(y),";")[[1]][1]))
+GeneSymbol$gene <- gene
+
+if(T){
+## 5.5 down genes ----
+down_genes <- subset(GeneSymbol, logFC < -cutoff)
+
+# 设置数据库 
+GO_database <- 'org.Hs.eg.db'  # GO是org.Hs.eg.db数据库
+KEGG_database <- 'hsa'         # KEGG是hsa数据库
+
+# gene ID转换 
+gene <- bitr(down_genes$gene, fromType = 'SYMBOL', toType = 'ENTREZID', OrgDb = GO_database)
+
+### 5.5.1 GO ----
+# GO富集分析
+kkd <- enrichGO(gene = gene$ENTREZID, # 导入基因的ENTREZID编号
+                OrgDb = GO_database, # 用到的数据库（人类是：org.Hs.eg.db）
+                keyType = "ENTREZID", # 设定读取的gene ID类型
+                ont = "ALL", # (ont为ALL因此包括 Biological Process,Cellular Component,Mollecular Function三部分）
+                pvalueCutoff = 0.05,
+                qvalueCutoff = 0.2)# 设定q值阈值
+
+### 5.5.2 KEGG ----
+# KEGG富集分析
+kk <- enrichKEGG(gene = gene$ENTREZID,
+                 keyType = "kegg",
+                 organism = KEGG_database,
+                 pAdjustMethod = "BH",
+                 pvalueCutoff = 0.05,
+                 qvalueCutoff = 0.2)
+
+## GO、KEGG结果整合 
+result <- list(enrichGO = kkd, enrichKEGG = kk)
+
+# 结果标记为下调 
+result_down <- result
+kkd_down <- result_down$enrichGO
+kk_down <- result_down$enrichKEGG
+
+### 5.5.3 down res_output ----
+# 导出下调enrichGO 
+write.csv(kkd_down@result, file = paste0(dir_enrich, "/GO_down.csv"), quote = F, row.names = F)
+
+# dotplot
+pdf(file = paste0(dir_enrich, "/GO_down.pdf"), width = 6, height = 7)
+p1 <- dotplot(kkd_down, showCategory = 5, split = "ONTOLOGY") + facet_grid(ONTOLOGY ~ ., scale = 'free', space = 'free') 
+print(p1)
+dev.off()
+
+# 导出下调enrichKEGG
+write.csv(kk_down@result, file = paste0(dir_enrich, "/KEGG_down.csv"), quote = F, row.names = F)
+
+# dotplot
+pdf(file = paste0(dir_enrich, "/KEGG_down.pdf"), width = 6, height = 5)
+p2 <- dotplot(kk_down,showCategory = 10)
+print(p2)
+dev.off()
+}
+
+if(T){
+## 5.6 up genes ----
+up_genes <- subset(GeneSymbol, logFC > cutoff)
+
+### 5.6.1 GO-up ----
+# gene ID转换 
+gene <- bitr(up_genes$gene, fromType = 'SYMBOL', toType = 'ENTREZID', OrgDb = GO_database)
+
+# GO富集分析
+kkd <- enrichGO(gene = gene$ENTREZID, # 导入基因的ENTREZID编号
+                OrgDb = GO_database, # 用到的数据库（人类是：org.Hs.eg.db）
+                keyType = "ENTREZID", # 设定读取的gene ID类型
+                ont = "ALL", # (ont为ALL因此包括 Biological Process,Cellular Component,Mollecular Function三部分
+                pvalueCutoff = 0.05,
+                qvalueCutoff = 0.2, # 设定q值阈值
+                readable = T)
+### 5.6.2 KEGG-up ----
+
+# KEGG富集分析
+kk <- enrichKEGG(gene = gene$ENTREZID,
+                 keyType = "kegg",
+                 organism = KEGG_database,
+                 pAdjustMethod = "BH",
+                 pvalueCutoff = 0.05,
+                 qvalueCutoff = 0.2)
+
+# GO、KEGG结果整合
+result <- list(enrichGO = kkd, enrichKEGG = kk)
+
+# 结果标记为上调
+result_up <- result
+kkd_up <- result_up$enrichGO
+kk_up <- result_up$enrichKEGG
+
+### 5.6.3 up res_output ----
+
+# 导出上调enrichGO
+write.csv(kkd_up@result, file = paste0(dir_enrich, "/GO_up.csv"), quote = F, row.names = F)
+
+# dotplot
+pdf(file = paste0(dir_enrich, "/GO_up.pdf"), width = 6, height = 7)
+p3 <- dotplot(kkd_up, showCategory = 5, split = "ONTOLOGY") + facet_grid(ONTOLOGY ~ ., scale = 'free', space = 'free')
+print(p3)
+dev.off()
+
+# 导出上调enrichKEGG
+write.csv(kk_up@result, file = paste0(dir_enrich, "/KEGG_up.csv"), quote = F, row.names = F)
+
+# dotplot
+pdf(file = paste0(dir_enrich, "/KEGG_up.pdf"), width = 6, height = 5)
+p4 <- dotplot(kk_up,showCategory = 10)
+print(p4)
+dev.off()
+}
+
+## 5.7 统计上下调的通路的数量 ----
+# 下调GO 
+table(kkd_down@result$p.adjust<0.05)
+# 下调KEGG 
+table(kk_down@result$p.adjust<0.05)
+# 上调GO 
+table(kkd_up@result$p.adjust<0.05)
+# 上调KEGG 
+table(kk_up@result$p.adjust<0.05)
+
+# 6. Selected pathway ----
+
+kegg <- kk_up@result[kk_up@result$pvalue<0.05,]
+p2 <- ggplot(kegg,aes(x=GeneRatio,y=Description))+
+      geom_point(aes(size=Count,color= -log10(pvalue)))+
+      theme_bw()+labs(y="",x="GeneRatio")+ 
+      scale_color_gradient(low="blue",high="red")+
+  #scale_size_continuous(range = c(3, 12))+  # 调整气泡的大小范围
+      theme(axis.text.y = element_text(angle = 0, hjust = 1))  # 调整Y轴标签角度
+
+ggsave(plot = p2,filename = "./03_Result/GO&KEGG/MOLM13/6W_vs_WT/downkegg.pdf")
+
+
+# 6.heatmap------
+library(reshape2)
+library(ComplexHeatmap)
+library(openxlsx)
+library(gtable)
+library(grid)
+## df structure
+# gene    module
+# PCDH9   tan
+# ALX1    tan
+# CGREF1  darkorange
+# 
+df <- data.frame(gene = colnames(cleanedData), module = mergedColors)
+df$gene <- gsub(".*\\|", "", df$gene)
+kegg_list <- list()
+i <- 0
+all_paths <- c()
+for (module in unique(df$module)) {
+  i <- i + 1
+  genes <- df[df$module == module,]$gene
+  trans_id <- bitr(genes, 
+                   fromType = "SYMBOL",
+                   toType = "ENTREZID",
+                   OrgDb = "org.Hs.eg.db")
+  kegg <- enrichKEGG(gene = trans_id$ENTREZID, organism = "hsa", 
+                     keyType = "kegg", 
+                     qvalueCutoff = 0.05)
+  result <- kegg@result
+  result$module <- module
+  kegg_list[[i]] <- result
+  significant_paths <- result[result$pvalue < 0.05, "Description"]
+  all_paths <- c(all_paths, significant_paths)
+  #all_paths <- c(all_paths, kegg@result$Description[c(1,2)])
+  #all_paths <- c(all_paths, kegg@result$Description[1])
+}
+
+paths <- c("PI3K-Akt signaling pathway", "MAPK signaling pathway")
+all_paths <- unique(c(all_paths, paths))
+kegg_all <- do.call(rbind, kegg_list)
+kegg_all <- kegg_all[kegg_all$Description %in% all_paths,]
+plot_data <- dcast(kegg_all[,c("Description", "pvalue", "module")], Description ~ module, value.var = "pvalue", fill = 1)
+
+write.xlsx(plot_data, file = "03_result/WGCNA/KEGGplotdata.xlsx",sep=",", quote=F)
+plot_data <- read_excel("03_result/WGCNA/KEGGplotdataselected.xlsx")
+plot_data<- as.data.frame(plot_data)
+rownames(plot_data) <- plot_data$Description
+plot_data$Description <- NULL
+plot_data <- -log(plot_data)
+plot_data[plot_data>5] <- 5
+#
+
+colnames(plot_data) <- paste0("Cluster", seq(ncol(plot_data)))
+plot_data <- as.matrix(plot_data)
+pdf(file =  "03_result/WGCNA/10.pathwayheatmap.pdf", width = 10, height = 10)
+p<- pheatmap(plot_data,name =" -log(pvalue)",
+             cluster_cols = T,
+             cluster_rows = F,
+             color = colorRampPalette(c("white", "blue"))(100))
+             
