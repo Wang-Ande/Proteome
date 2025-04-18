f917b32e2ad6ccc5aa6ef935803fdf1a2fb38163
Wangdali2049 <Ande2049@163.com>
2025-04-18 16:29:27 +0800

    feat:通路富集部分新增所有差异基因做富集chunk

--- 02_Code/Analysis_pipeline.R
+++ 02_Code/Analysis_pipeline.R
@@ -116,10 +116,10 @@ table(result_merge$result_merge$Sig)
 
 # 5. GO&KEGG ----
 ## 5.1 Set output catagory----
-dir_enrich <- "./03_Result/GO&KEGG/P53/P53_WT/"
+dir_enrich <- "./03_Result/GO&KEGG/OCI_AML2/Low_vs_Con/"
 
 ## 5.2 DE_res input ----
-DP_result <- read.csv('./03_Result/DEP/P53/P53_WT_vs_Ctrl/result_DE.csv')
+DP_result <- read.csv('./03_Result/DEP/OCI_AML2/Low_vs_Ctrl/result_DE.csv')
 
 ## 5.3 set P.Value ----
 GeneSymbol <- subset(DP_result, P.Value < 0.05)
@@ -132,13 +132,13 @@ y <- GeneSymbol$Genes
 gene <- unlist(lapply(y,function(y) strsplit(as.character(y),";")[[1]][1]))
 GeneSymbol$gene <- gene
 
-if(T){
-## 5.5 down genes ----
-down_genes <- subset(GeneSymbol, logFC < -cutoff)
-
 # 设置数据库 
 GO_database <- 'org.Hs.eg.db'  # GO是org.Hs.eg.db数据库
 KEGG_database <- 'hsa'         # KEGG是hsa数据库
+
+if(T){
+## 5.5 down genes ----
+down_genes <- subset(GeneSymbol, logFC < -cutoff)
 
 # gene ID转换 
 gene <- clusterProfiler::bitr(down_genes$gene, fromType = 'SYMBOL', toType = 'ENTREZID', OrgDb = GO_database)
@@ -253,7 +253,64 @@ print(p4)
 dev.off()
 }
 
-## 5.7 The number of up and down pathways ----
+if(T){
+  ## 5.7 All DE genes ----
+  All_genes <- subset(GeneSymbol, abs(logFC) > cutoff)
+  
+  ### 5.7.1 GO ----
+  # gene ID转换 
+  gene <- clusterProfiler::bitr(All_genes$gene, fromType = 'SYMBOL', toType = 'ENTREZID', OrgDb = GO_database)
+  
+  # GO富集分析
+  go <- clusterProfiler::enrichGO(gene = gene$ENTREZID, 
+                                  OrgDb = GO_database, 
+                                  keyType = "ENTREZID", 
+                                  ont = "ALL", 
+                                  pvalueCutoff = 0.05,
+                                  qvalueCutoff = 1, 
+                                  readable = T)
+  ### 5.7.2 KEGG ----
+  # KEGG富集分析
+  kegg <- clusterProfiler::enrichKEGG(gene = gene$ENTREZID,
+                                      keyType = "kegg",
+                                      organism = KEGG_database,
+                                      pAdjustMethod = "BH",
+                                      pvalueCutoff = 0.05,
+                                      qvalueCutoff = 1)
+  
+  # GO、KEGG结果整合
+  result <- list(enrichGO = go, enrichKEGG = kegg)
+  
+  # 结果标记为上调
+  result_up <- result
+  GO <- result_up$enrichGO
+  KEGG <- result_up$enrichKEGG
+  
+  ### 5.7.3 res_output ----
+  
+  # 导出上调enrichGO
+  write.csv(GO@result, file = paste0(dir_enrich, "/GO_all_DE.csv"), quote = F, row.names = F)
+  
+  # dotplot
+  pdf(file = paste0(dir_enrich, "/GO_all_DE.pdf"), width = 6, height = 7.5) # 如果用重叠 width = 6.5, height = 8.5 
+  p5 <- dotplot(GO, showCategory = 5, split = "ONTOLOGY") + 
+    facet_grid(ONTOLOGY ~ ., scale = 'free', space = 'free') +
+    theme(axis.text.y = element_text(angle = 0, hjust = 1)) +
+    scale_y_discrete(labels = function(x) str_wrap(x, width = 35))  # 控制每行最多显示40个字符
+  print(p5)
+  dev.off()
+  
+  # 导出上调enrichKEGG
+  write.csv(KEGG@result, file = paste0(dir_enrich, "/KEGG_all_DE.csv"), quote = F, row.names = F)
+  
+  # dotplot
+  pdf(file = paste0(dir_enrich, "/KEGG_all_DE.pdf"), width = 6, height = 5)
+  p6 <- dotplot(KEGG,showCategory = 10)
+  print(p6)
+  dev.off()
+}
+
+## 5.8 The number of up and down pathways ----
 # 下调GO 
 table(GO_down@result$p.adjust<0.05)
 # 下调KEGG 
