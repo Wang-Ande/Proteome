cc9f39d8298de948e03ce73dd0c066608121375d
Wangdali2049 <Ande2049@163.com>
2025-04-18 16:30:40 +0800

    docs：整理Venn_pre的代码

--- 02_Code/Venn prepratation.R
+++ 02_Code/Venn prepratation.R
@@ -9,15 +9,17 @@ setwd("../AML_project/aml_analysis")
 # 2. Library packages ----------------------------------------------------------
 library(dplyr)
 library(openxlsx)
+library(readr)
 
 # 3. Creat output category -----------------------------------------------------
 dir.create("03_result/Veen/Transcripton&Proteome/")
-dir <- "./03_Result/Venn/GSEA/"
+dir_venn <- "./03_Result/Venn/DEPs/Prote/P53/"
 
 # 4. Data input ----------------------------------------------------------------
-data1 <- read.xlsx("./03_Result/GSEA/MOLM13/2W_VS_WT/KEGG_results_selected.xlsx")
-data2 <- read.xlsx("./03_Result/GSEA/MOLM13/6W_VS_WT/KEGG_results_selected.xlsx")
-data3 <- read.xlsx("./03_Result/GSEA/MV4_11/2W_VS_WT/KEGG_results_selectd.xlsx")
+data1 <- read.csv("./03_Result/DEP/P53/P53_Mut_vs_Ctrl/result_DE.csv")
+data2 <- read.csv("./03_Result/DEP/P53/P53_WT_vs_Ctrl/result_DE.csv")
+data3 <- read.csv("./03_Result/DEP/MOLM13/Low_vs_Ctrl/result_DE.csv")
+
 data4 <- read.xlsx("./03_Result/GSEA/MV4_11/2W_VS_WT/KEGG_results_selectd.xlsx")
 data5 <- read.xlsx("./03_Result/GSEA/OCI_M2/2W_VS_WT/KEGG_results_selected.xlsx")
 data6 <- read.xlsx("./03_Result/GSEA/OCI_M2/6W_VS_WT/KEGG_results_selected.xlsx")
@@ -58,21 +60,18 @@ write.xlsx(All_down, "./03_Result/Venn/GSEA/All_down.xlsx")
 if(T){
   # 5. Select gene names（symbol） -----------------------------------------------
   #Group1
-  rownames(data1)<-data1$Row.names
-  y <- rownames(data1)
-  gene <- unlist(lapply(y,function(y) strsplit(as.character(y),"\\|")[[1]][2]))
+  y <- data1$Genes
+  gene <- unlist(lapply(y,function(y) strsplit(as.character(y),";")[[1]][1]))
   id<-data.frame(gene)
   data1<-cbind(id,data1)
   #Group2
-  rownames(data2)<-data2$Row.names
-  y <- rownames(data2)
-  gene <- unlist(lapply(y,function(y) strsplit(as.character(y),"\\|")[[1]][2]))
+  y <- data2$Genes
+  gene <- unlist(lapply(y,function(y) strsplit(as.character(y),";")[[1]][1]))
   id<-data.frame(gene)
   data2<-cbind(id,data2)
   #Group3
-  rownames(data3)<-data3$Row.names
-  y <- rownames(data3)
-  gene <- unlist(lapply(y,function(y) strsplit(as.character(y),"\\|")[[1]][2]))
+  y <- data3$Genes
+  gene <- unlist(lapply(y,function(y) strsplit(as.character(y),";")[[1]][1]))
   id<-data.frame(gene)
   data3<-cbind(id,data3)
   #Group4
@@ -92,54 +91,6 @@ if(T){
   data6<-cbind(id,data6)
 }
 
-# 加Sig列
-# logfc和pvalue需要更改！！！！！！！！！！！！！！！！！！！！！！！！！！！！
-if(T){
-  # group1
-  logFC = 1
-  P.Value = 0.05
-  k1 <- (data1$P.Value < P.Value)&(data1$logFC < -logFC) 
-  k2 <- (data1$P.Value < P.Value)&(data1$logFC > logFC)
-  data1 <- mutate(data1,Sig = ifelse(k1,"down",
-                                     ifelse(k2,"up","stable")))
-  # group2
-  logFC = 1
-  P.Value = 0.05
-  k1 <- (data2$P.Value < P.Value)&(data2$logFC < -logFC) 
-  k2 <- (data2$P.Value < P.Value)&(data2$logFC > logFC)
-  data2 <- mutate(data2,Sig = ifelse(k1,"down",
-                                     ifelse(k2,"up","stable")))
-  # group3
-  logFC = 1
-  P.Value = 0.05
-  k1 <- (data3$P.Value < P.Value)&(data3$logFC < -logFC) 
-  k2 <- (data3$P.Value < P.Value)&(data3$logFC > logFC)
-  data3 <- mutate(data3,Sig = ifelse(k1,"down",
-                                     ifelse(k2,"up","stable")))
-  # group4
-  logFC = 0.585
-  P.Value = 0.05
-  k1 <- (data4$P.Value < P.Value)&(data4$logFC < -logFC) 
-  k2 <- (data4$P.Value < P.Value)&(data4$logFC > logFC)
-  data4 <- mutate(data4,Sig = ifelse(k1,"down",
-                                     ifelse(k2,"up","stable")))
-  # group5
-  logFC = 0.585
-  P.Value = 0.05
-  k1 <- (data5$P.Value < P.Value)&(data5$logFC < -logFC) 
-  k2 <- (data5$P.Value < P.Value)&(data5$logFC > logFC)
-  data5 <- mutate(data5,Sig = ifelse(k1,"down",
-                                     ifelse(k2,"up","stable")))
-  # group6
-  logFC = 0.585
-  P.Value = 0.05
-  k1 <- (data6$P.Value < P.Value)&(data6$logFC < -logFC) 
-  k2 <- (data6$P.Value < P.Value)&(data6$logFC > logFC)
-  data6 <- mutate(data6,Sig = ifelse(k1,"down",
-                                     ifelse(k2,"up","stable")))
-}
-table(data1$Sig)
-table(data4$Sig)
 
 # 6. Extract subset ------------------------------------------------------------
 # change名需要更改 ！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
@@ -147,18 +98,18 @@ if(T){
   #Group1
   up_geneSet1 <- data1[data1$Sig == "up",c(1,ncol(data1))]
   down_geneSet1 <- data1[data1$Sig == "down",c(1,ncol(data1))]
-  up_geneSet1$Sig <- "2W_UP"
-  down_geneSet1$Sig <- "2W_DOWN"
+  up_geneSet1$Sig <- "P53-Mut"
+  down_geneSet1$Sig <- "P53-Mut"
   #Group2
   up_geneSet2 <- data2[data2$Sig == "up",c(1,ncol(data2))]
   down_geneSet2 <- data2[data2$Sig == "down",c(1,ncol(data2))]
-  up_geneSet2$Sig <- "6W_UP"
-  down_geneSet2$Sig <- "6W_DOWN"
+  up_geneSet2$Sig <- "P53-WT"
+  down_geneSet2$Sig <- "P53-WT"
   #Group3
   up_geneSet3 <- data3[data3$Sig == "up",c(1,ncol(data3))]
   down_geneSet3 <- data3[data3$Sig == "down",c(1,ncol(data3))]
-  up_geneSet3$Sig <- "Transcription_VEN_vs_WT"
-  down_geneSet3$Sig <- "Transcription_VEN_vs_WT" 
+  up_geneSet3$Sig <- "MOLM13_High-WT"
+  down_geneSet3$Sig <- "MOLM13_High-WT" 
   #Group4
   up_geneSet4 <- data4[data4$Sig == "up",c(1,ncol(data4))]
   down_geneSet4 <- data4[data4$Sig == "down",c(1,ncol(data4))]
@@ -179,18 +130,20 @@ if(T){
 # 7. Merge subset --------------------------------------------------------------
 if(T){
   # 上调基因
-  All_Up_geneSet <- bind_rows(up_geneSet4, up_geneSet5)
+  All_Up_geneSet <- bind_rows(up_geneSet1, up_geneSet2)
   All_Up_geneSet <- All_Up_geneSet %>% rename(Group=Sig)
   # 下调基因
-  All_Down_geneSet <- bind_rows(down_geneSet4,down_geneSet5)
+  All_Down_geneSet <- bind_rows(down_geneSet1,down_geneSet2)
   All_Down_geneSet <- All_Down_geneSet %>% rename(Group=Sig)
   
   # 8. Result output -------------------------------------------------------------
   # 命名前方的前缀“1”表示差异基因为取cutoff=1的结果
-  write.xlsx(All_Up_geneSet,file = paste0(dir,"All_UpGeneSet.xlsx"))
-  write.xlsx(All_Down_geneSet,file = paste0(dir,"All_DownGeneSet.xlsx"))
+  write.xlsx(All_Up_geneSet,file = paste0(dir_venn,"All_UpGeneSet.xlsx"))
+  write.xlsx(All_Down_geneSet,file = paste0(dir_venn,"All_DownGeneSet.xlsx"))
 }
 table(All_Up_geneSet$Group)
 table(All_Down_geneSet$Group)
 
-
+# Venn Logfc Top20 ----
+# load data
+read.csv("./03_Result/DEP/MOLM13/")
